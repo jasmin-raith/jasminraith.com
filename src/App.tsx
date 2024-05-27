6ecdbe07b17868d071506f7050cb221c1783a4ae
Jasmin Raith <jasminraith@icloud.com>
2024-05-27 14:40:48 +0200

    prepare basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
+import Home from './Home'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+      <Home/>
   )
 }
 
---
+++ src/Home.tsx
@@ -0,0 +1,21 @@
+function Home() {
+  const lila = "#A0357E"
+  const yellow = "#F5D000"
+  /* const pink = "#F400A8"
+  const blue = "#00EEF5"
+  const green = "#3B7475" */
+
+
+  return (
+    <>
+    {/* Header */}
+    <div style={{backgroundColor: lila}} className="fullscreen">
+      <p style={{color: yellow}}>Test</p>
+    </div>
+    {/* Project Preview */}
+    <div style={{backgroundColor: yellow}} className="fullscreen">Test 2</div>
+    </>
+  );
+}
+
+export default Home;
