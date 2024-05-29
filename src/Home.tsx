7c69c81b27bcceb5e27c3e4219a63eb9d8a8f282
Jasmin Raith <jasminraith@icloud.com>
2024-05-29 16:24:59 +0200

    optimize desktop header

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
 function Home() {
@@ -11,6 +12,8 @@ function Home() {
   const backgroundColors = [yellow, pink, blue, green];
 
   const projectPreviews: number[] = [1, 2, 3, 4, 5];
+  const [isMd] = useState(window.innerWidth >= 768);
+
 
   return (
     <>
@@ -19,24 +22,24 @@ function Home() {
       <div style={{ backgroundColor: lila }} className="fullscreen">
       <Container>
           <Row style={{height: '90vh'}}>
-            <Col xs={12} md={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '100px'}}>
-              <p style={{ color: blue, fontSize: 54 }}>Junior</p>
+            <Col sm={12} md={8} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '100px'}}>
+              <p style={{ color: blue, fontSize: '2rem' }}>Junior</p>
               <h1>
-                <p style={{ color: blue, fontSize: 108 }}>Webentwicklerin</p>
-                <p style={{ color: "white", fontSize: 108 }}>Angular/ React</p>
+                <p style={{ color: blue, fontSize: '4rem' }}>Webentwicklerin</p>
+                <p style={{ color: "white", fontSize: '2rem' }}>Angular/ React</p>
               </h1>
-            <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '80px', marginRight: '420px' }}>
-              <Button style={{ backgroundColor: blue, color: "black", borderColor: blue, width: '300px', fontSize: 27 }} variant="primary" size="lg">LinkedIn</Button>{' '}
-              <Button style={{ color: blue, textDecoration: 'none', width: '300px', fontSize: 27 }} variant="link" size="lg"><b>GitHub</b></Button>
+            <Col style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+              <Button style={{ backgroundColor: blue, color: "black", borderColor: blue, width: '300px', fontSize: '1rem' }} variant="primary" size="lg">LinkedIn</Button>{' '}
+              <Button style={{ color: blue, textDecoration: 'none', width: '300px', fontSize: '1rem' }} variant="link" size="lg"><b>GitHub</b></Button>
             </Col>
             </Col>
-            <Col xs={12} md={4} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-              <img style={{ width: "85%", paddingTop: '70px' }} src="/assets/img/Jasmin.png" />
+            <Col sm={12} md={4} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <img style={{ width: "85%", paddingTop: isMd ? '70px' : '0px' }} src="/assets/img/Jasmin.png" />
             </Col>
           </Row>
           <Row style={{height: '10vh'}}>
             <Col style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-              <p style={{ color: blue, fontSize: 27 }}>Meine Projekte</p>
+              <p style={{ color: blue, fontSize: '1rem' }}>Meine Projekte</p>
               <img src="/assets/img/arrow.svg" />
             </Col>
           </Row>
