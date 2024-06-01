0402c4bd10b607d594ce1b21e7619f72edaf6b97
Jasmin Raith <jasminraith@icloud.com>
2024-06-01 15:00:09 +0200

    add more projects and optimize header screen

--- src/Home.tsx
+++ src/Home.tsx
@@ -35,7 +35,7 @@ function Home() {
       gifVideo: "https://github.com/jasmin-raith/media/blob/main/katzenquiz/Katzenquiz.gif?raw=true",
     },
     {
-      projectName: "Tic-Tac-Toe", 
+      projectName: "Tic Tac Toe", 
       projectMadeWith: "JavaScript, HTML, CSS",
       tryButtonText: "Jetzt spielen",
       mediaUrl: "https://jasmin-raith.github.io/tic-tac-toe/",
@@ -44,13 +44,40 @@ function Home() {
       gifVideo: "https://github.com/jasmin-raith/media/blob/main/tic-tac-toe/tic-tac-toe.gif?raw=true",
     },
     {
-      projectName: "Lieferando-replica",
+      projectName: "Lieferando Nachbau",
       projectMadeWith: "JavaScript, HTML, CSS",
       tryButtonText: "Jetzt ansehen",
       mediaUrl: "https://jasmin-raith.github.io/lieferando-replica/",
       codeButtonText: "Code ansehen",
       codeUrl: "https://github.com/jasmin-raith/lieferando-replica",
       gifVideo: "https://github.com/jasmin-raith/media/blob/main/lieferando/lieferando.gif?raw=true",
+    },
+    {
+      projectName: "Synonym Finder",
+      projectMadeWith: "JavaScript, HTML, CSS, API",
+      tryButtonText: "Jetzt ausprobieren",
+      mediaUrl: "https://jasmin-raith.github.io/synonym-finder/",
+      codeButtonText: "Code ansehen",
+      codeUrl: "https://github.com/jasmin-raith/synonym-finder",
+      gifVideo: "https://github.com/jasmin-raith/media/blob/main/synonymfinder/Synonymfinder.gif?raw=true",
+    },
+    {
+      projectName: "Exact Match Mockup",
+      projectMadeWith: "Figma, Components",
+      tryButtonText: "Jetzt ansehen",
+      mediaUrl: "https://www.figma.com/proto/mDlUyNjnF3FCrsJJ6zYTQ6/Exact-Match?page-id=0%3A1&type=design&node-id=1-2&viewport=-445%2C403%2C0.29&t=ixUvjr69OJzp4P4S-1&scaling=scale-down&starting-point-node-id=1%3A2",
+      codeButtonText: "Übersicht ansehen",
+      codeUrl: "https://www.figma.com/design/mDlUyNjnF3FCrsJJ6zYTQ6/Exact-Match?m=dev&node-id=0-1&t=c5mwxNX5LclzQkJ3-1",
+      gifVideo: "https://github.com/jasmin-raith/media/blob/main/exact-match/exact-match.gif?raw=true",
+    },
+    {
+      projectName: "Idee für einen virtuellen Dm Shop",
+      projectMadeWith: "Figma, Components",
+      tryButtonText: "Jetzt ansehen",
+      mediaUrl: "https://www.figma.com/proto/nDjJmsPiMIXRWlpeaWmMFz/Virtueller-Dm-Shop?page-id=0%3A1&type=design&node-id=1-2&viewport=3%2C62%2C0.45&t=HFtJz7rNpHropiwQ-1&scaling=scale-down&starting-point-node-id=1%3A2",
+      codeButtonText: "Übersicht ansehen",
+      codeUrl: "https://www.figma.com/design/nDjJmsPiMIXRWlpeaWmMFz/Virtueller-Dm-Shop?m=dev&node-id=0-1&t=VfsQjOwoYUHwAln9-1",
+      gifVideo: "https://github.com/jasmin-raith/media/blob/main/concept-idea-virtual-dm-shop/concept-idea-virtual-dm-shop.gif?raw=true",
     }
   ];
   
@@ -65,11 +92,11 @@ function Home() {
       <div>
           <div style={{height: '90vh', display:'flex', flexDirection: isMd ? 'row' : 'column', alignContent: 'flex-start', margin: isMd ? '0px 54px' : '0px 27px'}}>
             {/* Text und Buttons */}
-            <div style={{ height: 'fit-content', display: 'flex', width: isMd ? '60%' : '100%', flexDirection: 'column', justifyContent: 'center', overflow: 'hidden'}}>
+            <div style={{ height: 'fit-content', display: 'flex', width: isMd ? '60%' : '100%', flexDirection: 'column', justifyContent: 'center'}}>
               <p style={{ color: blue, fontSize: '2rem', marginTop:'100px' }}>Junior</p>
               <h1>
-                <p style={{ color: blue, fontSize: '4rem', wordWrap: 'break-word', hyphens: 'auto'}} lang="en">Webentwicklerin</p>
-                <p style={{ color: "white", fontSize: '2rem' }}>Angular/ React</p>
+                <p style={{ color: blue, fontSize: '4rem', lineHeight: '100%'}} lang="en">Frontend Entwicklerin</p>
+                <p style={{ color: "white", fontSize: '2rem', marginTop: '12px' }}>React / Angular / TypeScript</p>
               </h1>
               <div style={{ display: 'flex', flexDirection: 'column', marginTop: '50px' }}>
                 <Button style={{ backgroundColor: blue, color: "black", borderColor: blue, width: '300px', fontSize: '1rem' }} variant="primary" size="lg">LinkedIn</Button>{' '}
