265fa3a30d65f037d93edfe5c7e4110700fa35ca
Jasmin Raith <jasminraith@icloud.com>
2024-06-03 18:25:37 +0200

    Test with overflow hidden

--- src/Home.tsx
+++ src/Home.tsx
@@ -112,31 +112,29 @@ function Home() {
     <>
       {/* Header */}
       
-      <div style={{ backgroundColor: lila }} className="fullscreen">
-      <div>
-          <div style={{height: '90vh', display:'flex', flexDirection: isMd ? 'row' : 'column', alignContent: 'flex-start', margin: '0px 54px'}}>
-            {/* Text und Buttons */}
-            <div style={{ height: 'fit-content', display: 'flex', width: isMd ? '60%' : '100%', flexDirection: 'column', justifyContent: 'center'}}>
-              <p style={{ color: blue, fontSize: '2rem', marginTop:'100px' }}>Junior</p>
-              <h1>
-                <p style={{ color: blue, fontSize: '4rem', lineHeight: '100%'}} lang="en">Frontend Entwicklerin</p>
-                <p style={{ color: "white", fontSize: '2rem', marginTop: '12px' }}>React / Angular / TypeScript</p>
-              </h1>
-              <div style={{ display: 'flex', flexDirection: 'column', marginTop: '50px' }}>
-                <Button href="https://www.linkedin.com/in/jasminraith-dev/" target="_blank" rel="noopener noreferrer" style={{ backgroundColor: blue, color: "black", borderColor: blue, width: '300px', fontSize: '1rem' }} variant="primary" size="lg">LinkedIn</Button>{' '}
-                <Button href="https://github.com/jasmin-raith" target="_blank" rel="noopener noreferrer" style={{ color: blue, textDecoration: 'none', width: '300px', fontSize: '1rem' }} variant="link" size="lg"><b>GitHub</b></Button>
-              </div>
-            </div>
-            {/* Bild */}
-            <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden'}}>
-              <img style={{ height: '100%', objectFit: 'contain', paddingTop: isMd ? '70px' : '20px', overflow: 'hidden' }} src="/assets/img/Schnupper.png" />
+      <div style={{ backgroundColor: lila, overflow: 'hidden' }} className="fullscreen">
+        <div style={{height: '90vh', display:'flex', flexDirection: isMd ? 'row' : 'column', alignContent: 'flex-start', margin: '0px 54px'}}>
+          {/* Text und Buttons */}
+          <div style={{ height: 'fit-content', display: 'flex', width: isMd ? '60%' : '100%', flexDirection: 'column', justifyContent: 'center'}}>
+            <p style={{ color: blue, fontSize: '2rem', marginTop:'100px' }}>Junior</p>
+            <h1>
+              <p style={{ color: blue, fontSize: '4rem', lineHeight: '100%'}} lang="en">Frontend Entwicklerin</p>
+              <p style={{ color: "white", fontSize: '2rem', marginTop: '12px' }}>React / Angular / TypeScript</p>
+            </h1>
+            <div style={{ display: 'flex', flexDirection: 'column', marginTop: '50px' }}>
+              <Button href="https://www.linkedin.com/in/jasminraith-dev/" target="_blank" rel="noopener noreferrer" style={{ backgroundColor: blue, color: "black", borderColor: blue, width: '300px', fontSize: '1rem' }} variant="primary" size="lg">LinkedIn</Button>{' '}
+              <Button href="https://github.com/jasmin-raith" target="_blank" rel="noopener noreferrer" style={{ color: blue, textDecoration: 'none', width: '300px', fontSize: '1rem' }} variant="link" size="lg"><b>GitHub</b></Button>
             </div>
           </div>
-          <div style={{height: '10vh', display: 'flex', justifyContent: 'center'}}>
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-              <p style={{ color: blue, fontSize: '1rem' }}>Meine Projekte</p>
-              <img style={{ objectFit: 'cover' }} src="/assets/img/arrow.svg" />
-            </div>
+          {/* Bild */}
+          <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden'}}>
+            <img style={{ height: '100%', objectFit: 'contain', paddingTop: isMd ? '70px' : '20px', overflow: 'hidden' }} src="/assets/img/Schnupper.png" />
+          </div>
+        </div>
+        <div style={{height: '10vh', display: 'flex', justifyContent: 'center'}}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <p style={{ color: blue, fontSize: '1rem' }}>Meine Projekte</p>
+            <img style={{ objectFit: 'cover' }} src="/assets/img/arrow.svg" />
           </div>
         </div>
       </div>
@@ -150,6 +148,7 @@ function Home() {
             backgroundColor: backgroundColors[index % backgroundColors.length],
             display: 'flex',
             alignItems: 'center',
+            overflow: 'hidden',
           }}
           className="fullscreen"
         >
